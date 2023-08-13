e749a8561b3c4c73077b7db344a8d180fab7d160
Miguel <69534716+miguelcimolin@users.noreply.github.com>
2023-08-13 15:47:06 -0300

    Atualização das rotas.

--- src/routes/RoutesApp.jsx
+++ src/routes/RoutesApp.jsx
@@ -1,6 +1,10 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { Fragment } from 'react'
 import Home from '../pages/Home'
+import Config from '../pages/Config'
+import Dashboard from '../pages/Dashboard'
+import Relatorio from '../pages/Relatorio'
+import Login from '../pages/Login'
 
 export default function RoutesApp() {
 
@@ -8,8 +12,12 @@ export default function RoutesApp() {
         <BrowserRouter>
             <Fragment>
                 <Routes>                    
-                    <Route path="/" element={<Home />} />
-                    <Route path="*" element={<Home />} />
+                    <Route path="*" element={<Login />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/home" element={<Home />} />
+                    <Route path="/config" element={<Config />} />
+                    <Route path="/dashboard" element={<Dashboard />} />
+                    <Route path="/relatorio" element={<Relatorio />} />
                 </Routes>
             </Fragment>
         </BrowserRouter>
