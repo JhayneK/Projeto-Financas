c5bdf806a9ab86ea5b98ac084a2890815adf201a
Luigi Colonetti <luigimcolonetti@gmail.com>
2023-08-29 01:39:21 -0300

    Melhoria de responsividade na sidebar e header logado
    Ao apertar numa rota da sidebar o botão ficará marcado para saber que o usuário está naquela rota.
    Ao apertar em "perfil" no header de usuários logados é mostrado uma opção para deslogar do site.

--- src/components/Header/HeaderDeslogado.jsx
+++ src/components/Header/HeaderDeslogado.jsx
@@ -3,10 +3,14 @@ import { Link } from "react-router-dom"
 export default function HeaderLogado() {
     return(
         <div className="header">
-            <nav className="navbar">
-                <Link className="header-title" to="/" >Home</Link>
-                <Link className="header-title" to="/login">Login</Link>
-            </nav>
+            <div className="header-content">
+                <nav className="navbar">
+                    <Link className="header-title" to="/" >Home</Link>
+                </nav>
+                <nav className="navbar">
+                    <Link className="header-title" to="/login">Login</Link>
+                </nav>
+            </div>
         </div>
     )
 }
--- src/components/Header/HeaderLogado.jsx
+++ src/components/Header/HeaderLogado.jsx
@@ -1,14 +1,52 @@
-import { Link } from "react-router-dom"
+import React, { useState, useEffect, useRef } from "react";
 import { FiChevronDown } from "react-icons/fi";
 
 export default function HeaderLogado() {
-    return(
+    const [menuVisible, setMenuVisible] = useState(false);
+    const menuRef = useRef(null);
+
+    const toggleMenu = () => {
+        setMenuVisible(!menuVisible);
+    };
+
+    const closeMenu = () => {
+        setMenuVisible(false);
+    };
+
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+            if (menuRef.current && !menuRef.current.contains(event.target)) {
+                closeMenu();
+            }
+        };
+
+        if (menuVisible) {
+            document.addEventListener("mousedown", handleClickOutside);
+        } else {
+            document.removeEventListener("mousedown", handleClickOutside);
+        }
+
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside);
+        };
+    }, [menuVisible]);
+
+    return (
         <div className="header">
-            <nav className="navbar">
-                <Link className="header-title" to="/config">
-                    <span style={{display: "flex", alignItems: "center", justifyContent: "center"}}><FiChevronDown />Perfil</span>
-                </Link>
-            </nav>
+            <div className="header-content">
+                <nav className="navbar">
+                    <div className="header-title" onClick={toggleMenu}>
+                        <span style={{display: "flex", alignItems: "center"}}><FiChevronDown />Perfil</span>
+                    </div>
+                    {menuVisible && (
+                        <div className="header-menu-options" ref={menuRef}>
+                            <div className="header-options">
+                                <span>Sair</span>
+                            </div>
+                        </div>
+                    )}
+                </nav>
+            </div>
         </div>
-    )
+    );
 }
