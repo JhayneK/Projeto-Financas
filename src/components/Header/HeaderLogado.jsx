08a7d3ba13f162f8d120846b3f5e4a5d76d7fbbb
Luigi Colonetti <luigimcolonetti@gmail.com>
2023-09-01 01:37:22 -0300

    Melhoria no header (logado)
    Após 0.9 segundos que o usuário deixar o cursor fora do menu de opções em "Perfil"
    o menu será fechado sozinho. Também será fechado caso o usuário clique fora das opções do menu.

--- src/components/Header/HeaderLogado.jsx
+++ src/components/Header/HeaderLogado.jsx
@@ -1,21 +1,65 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { FiChevronDown } from "react-icons/fi";
 
 export default function HeaderLogado() {
     const [menuVisible, setMenuVisible] = useState(false);
+    const [mouseOverMenu, setMouseOverMenu] = useState(false);
     const menuRef = useRef(null);
+    const timeoutRef = useRef(null);
 
     const toggleMenu = () => {
         setMenuVisible(!menuVisible);
+        // Inicie o timer quando o menu for aberto
+        if (!menuVisible) {
+            timeoutRef.current = setTimeout(() => {
+                if (!mouseOverMenu) {
+                    closeMenu();
+                }
+            }, 900);  // 0.9 segundos
+        }
     };
 
     const closeMenu = () => {
         setMenuVisible(false);
+        clearTimeout(timeoutRef.current);
+    };
+
+    const handleTitleClick = () => {
+        if (menuVisible) {
+            closeMenu();
+        } else {
+            toggleMenu();
+        }
+    };
+
+    const handleMouseEnter = () => {
+        clearTimeout(timeoutRef.current);
+        setMouseOverMenu(true);
+    };
+
+    const handleMouseLeave = () => {
+        setMouseOverMenu(false);
+        // Não inicie o timer se o menu não estiver visível
+        if (menuVisible) {
+            timeoutRef.current = setTimeout(() => {
+                closeMenu();
+            }, 900);  // 0.9 segundos
+        }
     };
 
     useEffect(() => {
         const handleClickOutside = (event) => {
-            if (menuRef.current && !menuRef.current.contains(event.target)) {
+            if (
+                menuVisible &&
+                menuRef.current &&
+                !menuRef.current.contains(event.target)
+            ) {
+                closeMenu();
+            }
+        };
+
+        const handleDocumentClick = (event) => {
+            if (!menuRef.current.contains(event.target)) {
                 closeMenu();
             }
         };
@@ -35,11 +79,18 @@ export default function HeaderLogado() {
         <div className="header">
             <div className="header-content">
                 <nav className="navbar">
-                    <div className="header-title" onClick={toggleMenu}>
-                        <span style={{display: "flex", alignItems: "center"}}><FiChevronDown />Perfil</span>
+                    <div className="header-title" onClick={handleTitleClick}>
+                        <span style={{ display: "flex", alignItems: "center" }}>
+                            <FiChevronDown />Perfil
+                        </span>
                     </div>
                     {menuVisible && (
-                        <div className="header-menu-options" ref={menuRef}>
+                        <div
+                            className="header-menu-options"
+                            ref={menuRef}
+                            onMouseEnter={handleMouseEnter}
+                            onMouseLeave={handleMouseLeave}
+                        >
                             <div className="header-options">
                                 <span>Sair</span>
                             </div>
